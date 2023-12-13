17510496d6838061cde583f5fcbccbfad7c5d0fc
malleshoracle09@gmail.com <malleshoracle09@gmail.com>
2023-12-13 17:14:40 +0000

    commit

--- models/product_sales.sql
+++ models/product_sales.sql
@@ -1,3 +1,4 @@
+{{ config(materialized='table') }}
 with product_sales as(
 select city,state,sum(NA_SALES) na_sales,sum(global_sales) global_sales from PROD.product group by 1,2)
 select * from product_sales
